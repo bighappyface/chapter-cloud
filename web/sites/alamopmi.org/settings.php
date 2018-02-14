5b3b211ecbe4ecdd20d2b88b907f18a2569ed255
bighappyface <anomalous20@gmail.com>
2018-02-13 21:40:29 -0600

    fix(sites): add subsite for alamopmi.org

--- web/sites/alamopmi.org/settings.php
+++ web/sites/alamopmi.org/settings.php
@@ -34,7 +34,7 @@ $config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
 
 // Automatic Platform.sh settings.
 if (file_exists($app_root . '/' . $site_path . '/../settings.platformsh.php')) {
-  $platformsh_subsite_id = 'second';
+  $platformsh_subsite_id = 'alamopmi.org';
   include $app_root . '/' . $site_path . '/../settings.platformsh.php';
 }
 
